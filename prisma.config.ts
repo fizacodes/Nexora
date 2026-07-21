9d9f6e3af8afef711b2e0c55208c80ce3e76575a
Fiza Shahid <fiza34037@gmail.com>
2026-07-21 12:54:26 +0500

    Removed Seed.ts

--- prisma.config.ts
+++ prisma.config.ts
@@ -5,10 +5,6 @@ import { defineConfig } from "prisma/config";
 
 export default defineConfig({
   schema: "prisma/schema.prisma",
-  migrations: {
-     seed: "tsx prisma/seed.ts",
-    path: "prisma/migrations",
-  },
   datasource: {
     url: process.env["DATABASE_URL"],
   },
